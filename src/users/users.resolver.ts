9aad8fbf702be234f68c6e952e9c916788c1e311
Park Taehyeong <2468ab@naver.com>
2021-02-22 01:45:56 +0900

    2021-02-22 01:45

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
+import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
@@ -14,7 +15,26 @@ export class UsersResolver {
     }
 
     @Mutation(returns => CreateAccountOutput)
-    createAccount(@Args('input') createAccountInput: CreateAccountInput) {
+    async createAccount(@Args('input') createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
+        try {
+            return this.usersService.createAccount(createAccountInput);
+        } catch(error) {
+            return {
+                error,
+                ok: false,
+            };
+        }
+    }
 
+    @Mutation(returns=>LoginOutput)
+    async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+        try{
+            return this.usersService.login(loginInput); 
+        } catch(error) {
+            return {
+                ok: false,
+                error
+            };
+        }
     }
 }
